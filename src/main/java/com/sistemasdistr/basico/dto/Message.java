3ddf54e61eb9e5eca0d94913aae5b3896073d262
rrf1003 <rrf1003@alu.ubu.es>
2026-04-13 15:14:48 +0200

    Add Message File

---
+++ src/main/java/com/sistemasdistr/basico/dto/Message.java
@@ -0,0 +1,11 @@
+package com.sistemasdistr.basico.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Message {
+    private String from;
+    private String text;
+}
